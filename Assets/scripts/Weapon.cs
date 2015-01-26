c1753149769a007472e3dd5dcd2615f38eff04fd
Charidan <twilly0@gmail.com>
2015-01-25 16:11:31 -0800

    Revert "Revert "Abstract Weapon""
    This reverts commit 9f16de6d8bb75525023a4fe0fb277b0b09e2e2b1.

---
+++ Assets/scripts/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * The blueprint for weapons implementation. 
+ * Need to implement Start() and Fire() in children
+ */
+
+public abstract class Weapon : MonoBehaviour {
+	// Weapon Traits
+	// implement in children
+	protected int damage;
+	protected int maxClipSize;
+	protected int reloadSpeed;
+	protected int fireRate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// implement in children
+	// Information provided by player:
+	// 		Player Position, Reticule Position, Current Ammo
+	// 		Any modifiers associated with using the weapon
+	public abstract void Fire (ref player owner);
+
+	// included for later
+	public abstract void altFire (ref player owner);
+
+	// accessors
+	public int getDamage(){
+		return damage;
+	}
+
+	public int getClipSize(){
+		return maxClipSize;
+	}
+
+	public int getReloadSpeed(){
+		return reloadSpeed;
+	}
+
+	public int getFireRate(){
+		return fireRate;
+	}
+}
