66468cd9bfa4a11975c9a82f6a303a9b3f10510f
Nick Junius <njunius@ucsc.edu>
2015-01-25 17:09:12 -0800

    Abstract Projectile

---
+++ Assets/scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Projectile : MonoBehaviour {
+	
+	// Projectile traits
+	protected int damage;
+	protected player owner;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	// accessor functions
+	public int getProjectileDamage(){
+		return damage;
+	}
+	
+	// set functions
+	
+	public void setOwner(ref player newOwner){
+		owner = newOwner;
+	}
+}
