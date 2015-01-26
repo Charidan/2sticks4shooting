e390a8c8c2bb9f63f715f88571e6beb8be0424bb
Patrick Mathieu <pdmathieu5@gmail.com>
2015-01-25 16:53:19 -0800

    Gave player hp

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
-	// Initialize vectors
+	// Initialize player attributes
+	public float max_hp = 100.0f;
+	public float curr_hp = 100.0f;
 	public Vector2 speed = new Vector2(10, 10);
 	public Vector2 movement;
 
@@ -18,11 +20,21 @@ public class player : MonoBehaviour {
 
 		// Calculate the movement vector
 		movement = new Vector2(speed.x * inputX, speed.y * inputY);
+
+		// Test hp
+		if (Input.GetKeyUp (KeyCode.Space)) adj_hp(-1.0f);
 	}
 
 	void FixedUpdate() {
 		// Do the movement
 		rigidbody2D.velocity = movement;
+	}
+
+	void adj_hp(float adj) {
+		curr_hp += adj;
+		if(curr_hp < 0) curr_hp = 0;
+		if(curr_hp > max_hp) curr_hp = max_hp;
+		if(max_hp < 1) max_hp = 1;
 	}
 }
 
