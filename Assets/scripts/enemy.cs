0b731ba26a35a2290b3419a89558d903f3e18197
Patrick Mathieu <patrickmathieu@dhcp-203-131.cruznet.ucsc.edu>
2015-01-25 16:24:23 -0800

    Merged edits

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	// Initialize vectors
+	public Vector2 speed = new Vector2(10, 10);
+	public Vector2 movement;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		// Detect keys
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// Calculate the movement vector
+		movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate() {
+		// Do the movement
+		rigidbody2D.velocity = movement;
+	}
+}
+
