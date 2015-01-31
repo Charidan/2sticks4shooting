5d5767e87b3a04be7944edbe6c3ca362c0596cd5
Nick Junius <njunius@ucsc.edu>
2015-01-30 18:43:32 -0800

    Healthbar now functions properly
    The healthbar no longer rotates with the player and changes its sprite
    only when the midpoint between 2 divisible by 10 numbers is crossed.

--- Assets/scripts/HealthBar.cs
+++ Assets/scripts/HealthBar.cs
@@ -6,15 +6,16 @@ public class HealthBar : MonoBehaviour {
 	private Sprite[] healthSprites;
 	private float playerHP;
 	private Player owner;
+	private int healthSpriteIndex;
 	void Awake(){
 		// load each sprite into HealthBar for easy updating later
 		healthSprites = Resources.LoadAll<Sprite>("HealthBarSpriteSheet");
 	}
 	// Use this for initialization
 	void Start () {
-		//playerHP = 100;
 		owner = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player>();
 		playerHP = owner.getHP ();
+	    healthSpriteIndex = Mathf.RoundToInt (playerHP / 10);
 		transform.position = owner.transform.position;
 		Debug.Log (playerHP);
 	}
@@ -23,7 +24,16 @@ public class HealthBar : MonoBehaviour {
 	void Update () {
 		transform.position = owner.transform.position;
 		playerHP = owner.getHP ();
-		int healthSpriteIndex = Mathf.RoundToInt (playerHP / 10);
+
+		// change the sprite only when the player's health is
+		// less than half way between 2 numbers divisible by 10
+		if (Mathf.RoundToInt(playerHP) % 10 > 4) {
+						healthSpriteIndex = Mathf.CeilToInt (playerHP / 10);
+						Debug.Log (healthSpriteIndex);
+				} else {
+						healthSpriteIndex = Mathf.FloorToInt (playerHP / 10);
+						Debug.Log (healthSpriteIndex);
+				}
 		GetComponent<SpriteRenderer> ().sprite = healthSprites [healthSpriteIndex];
 	}
 }
