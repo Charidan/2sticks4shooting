b090ce7ee0f2a1c25986a2a58b8a3330a94671ec
Patrick Mathieu <pdmathieu5@gmail.com>
2015-02-03 11:31:49 -0800

    Created weapon pickup (unfinished) and started weapon list

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponManager : MonoBehaviour {
+
+	Weapon[] weapon_list = new Weapon[2];
+	/*
+	 * Weapon List index organization:
+	 * 0 - Pistol
+	 * ...
+	 */
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+}
---
+++ Assets/Scripts/WeaponPickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponPickup : MonoBehaviour {
+
+	// Weapon list index. Random with lower probability on more valuable weapons
+	int Weapon;
+
+	// Use this for initialization
+	void Start () {
+		// Define type of weapon pickup
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnTriggerEnter2D(Collider2D obj) {
+		if(obj.gameObject.tag == "Player") {
+			Debug.Log("Weapon Pickup");
+
+			// Transfer weapon attributes to destination from here
+
+			Destroy(gameObject);
+		}
+	}
+}
