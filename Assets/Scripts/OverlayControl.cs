2a53731e4fb465dbe5eb8ce6726ab0a7a48d1bb4
Nick Junius <njunius@ucsc.edu>
2015-03-04 23:14:45 -0800

    Overlay Now Properly tied to Player HP
    The overlay's opacity is now tied to the total player health of all
    players in the game.

--- Assets/Scripts/OverlayControl.cs
+++ Assets/Scripts/OverlayControl.cs
@@ -5,7 +5,13 @@ using System.Collections;
 public class OverlayControl : MonoBehaviour {
 
 	protected Sprite[] overlay;
-	protected bool toggleImage;
+	// uses GameObject as type due to FindGameObjectsWithTag() only returning GameObject[]
+	protected GameObject[] calc_hp;
+	protected int toggleImage;
+	protected int delay_counter;
+	protected int total_player_HP;
+	// used for converting total health to individual color values
+	protected float health_to_opacity;
 
 	void Awake()
 	{
@@ -15,18 +21,43 @@ public class OverlayControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		toggleImage = false;
+		toggleImage = 0;
+		delay_counter = 0;
+		calc_hp = null;
+		total_player_HP = 0;
+		health_to_opacity = 0.0f;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		// toggles between the two overlays every frame
-		if (toggleImage) {
-			GetComponent<Image> ().sprite = overlay[0];
-			toggleImage = !toggleImage;
-		} else {
-			GetComponent<Image> ().sprite = overlay[1];
-			toggleImage = !toggleImage;
+	void Update(){
+		// if total_player_HP is not reset, the value will only increase and not be an aggregate of the total health
+		total_player_HP = 0;
+		// used to get references to all player objects for oppacity calculation
+		calc_hp = GameObject.FindGameObjectsWithTag ("Player");
+		// if there are players, add their total health each frame
+		if (calc_hp != null) {
+			for(int i = 0; i < Player.getNumPlayers(); i++)	{
+				total_player_HP += calc_hp[i].GetComponent<Player>().getHP();
+			}
 		}
+
+
+		health_to_opacity = (total_player_HP / Player.getNumPlayers ()) / 10000f;
+		GetComponent<Image> ().color = new Color (1.0f, 1.0f, 1.0f, Mathf.Pow((1.0f - health_to_opacity)/2, 2.0f));
+	}
+
+	void FixedUpdate () {
+		if (delay_counter == 4) {
+			delay_counter = 0;
+			if(toggleImage < 2){
+				toggleImage++;
+			}else{
+				toggleImage = 0;
+			}
+		}
+
+		// toggles between the two overlays every 5 updates
+		GetComponent<Image> ().sprite = overlay[toggleImage];
+		delay_counter++;
 	}
 }
