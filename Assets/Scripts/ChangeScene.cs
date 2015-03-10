d5b128dab37bc67eede42b58a30e0f9f77080546
Nick Junius <njunius@ucsc.edu>
2015-03-10 08:45:32 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 451c59b22225131896b92f7809abe2bff109f42a.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	
+	// changes the scene to the scene of the name of the argument
+	// pre: the name corresponds to a valid scene
+	public void switchScene (string newSceneName) {
+		Application.LoadLevel (newSceneName);
+	}
+}
