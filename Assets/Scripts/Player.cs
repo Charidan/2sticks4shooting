6a2588523649ec66da5c099226053d8bae1361b2
Chaiz <ctuimolo@ucsc.edu>
2015-03-11 16:20:28 -0700

    Player sprite slightly modified
    + Changed to a more orthographic view with passable legless
    animations....

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour {
 	{
 		num_players = 0;
 		// load all frames in fruitsSprites array
-		pSprites = Resources.LoadAll<Sprite>("examplesheet");
+		pSprites = Resources.LoadAll<Sprite>("playersheet");
 	}
 
 	// Use this for initialization
@@ -202,23 +202,23 @@ public class Player : MonoBehaviour {
 		//face North
 		if (arcTan < 22.5 && arcTan >= -22.5)
 			//arcTan = 0;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [1];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [4];
 		//north-east
 		else if (arcTan < -22.5 && arcTan >= -67.5)
 			//arcTan = -45;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [2];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [3];
 		//east
 		else if (arcTan < -67.5 && arcTan >= -112.5)
 			//arcTan = -90;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [3];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [2];
 		//south-east
 		else if (arcTan < -112.5 && arcTan >= -157.5)
 			//arcTan = -135;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [4];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [1];
 		//south
 		else if (arcTan < -157.5 && arcTan >= -202.5)
 			//arcTan = -180;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [6];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [0];
 		//south-west
 		else if (arcTan < -202.5 && arcTan >= -247.5)
 			//arcTan = -225;
@@ -226,11 +226,11 @@ public class Player : MonoBehaviour {
 		//west
 		else if ((arcTan >= -270 && arcTan < -247.5) || (arcTan <= 90 && arcTan >= 67.5))
 			//arcTan = 90;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [8];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [6];
 		//north-west
 		else if (arcTan < 67.5 && arcTan >= 22.5)
 			//arcTan = 45;
-			GetComponent<SpriteRenderer> ().sprite = pSprites [0];
+			GetComponent<SpriteRenderer> ().sprite = pSprites [5];
 	}
 
 	// must be called once in Update() to allow for the weapons to correctly give the following attributes their correct values:
