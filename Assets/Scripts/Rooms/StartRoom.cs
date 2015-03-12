cbf2e3ae73ac89fca37dca81df73de1b2852e578
Nick Junius <njunius@ucsc.edu>
2015-03-11 18:32:27 -0700

    Rooms now work after reloading the level
    Rooms now can be rebuilt after the level is reloaded.

--- Assets/Scripts/Rooms/StartRoom.cs
+++ Assets/Scripts/Rooms/StartRoom.cs
@@ -6,6 +6,7 @@ namespace AssemblyCSharp
 
 		// Use this for initialization
 		void Start () {
+			FloorManager.resetFloor (1);
 			FloorGraph.singleton.put (new Vector2 (0, 0), this);
 			addUnexploredDoor (FloorManager.Direction.NORTH);
 			generateWalls();
