0b39678b9646bf6515c62960f5d627c6703ead81
Nick Junius <njunius@ucsc.edu>
2015-03-11 19:52:34 -0700

    Skip able introduction text added
    We now have a short introduction before players reach the main menu.

---
+++ Assets/Scripts/IntroTextEdit.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IntroTextEdit : MonoBehaviour {
+
+	int delay_text_appearance;
+	int delay_text_fade;
+	int text_fade;
+
+	// Use this for initialization
+	void Start () {
+		delay_text_appearance = 0;
+		delay_text_fade = 0;
+		text_fade = 480;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// lets user skip the scene
+		if (Input.anyKey) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+
+	void FixedUpdate(){
+		float old_alpha = GetComponent<Text>().color.a;
+		if (delay_text_appearance < 60) {
+			delay_text_appearance++;	
+		} else if( old_alpha < 1.0f && delay_text_fade == 0){
+			GetComponent<Text>().color = new Color(1.0f, 1.0f, 1.0f, old_alpha + 0.05f);
+		}
+
+		if (old_alpha >= 1.0f && delay_text_fade < text_fade) {
+			delay_text_fade++;
+		} else if (old_alpha > 0.0f && delay_text_fade >= text_fade) {
+			GetComponent<Text>().color = new Color(1.0f, 1.0f, 1.0f, old_alpha - 0.05f);
+		}
+
+		if(old_alpha <= 0.0f && delay_text_fade >= text_fade){
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
